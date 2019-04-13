86f6be67f45da4c5f406888f0676bace61361c6b
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-13 16:37:54 +0100

    Button positioned

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -16,23 +16,25 @@ const boxStyle = {
 class Main extends Component {
     render() {
       return (
-        <Grid container direction="column" justify="center" alignItems="center" style={{minHeight:'100vh'}} spacing={40}>
-            <Grid item xs={12}>
-            <Paper style={boxStyle} elevation={5}>
-                <Typography variant="h5" component="h3" align="center">
-                “We travel not to escape life, but for life not to escape us.”
-                </Typography>
-                <Typography variant="caption" align="right">
-                Anonymous
-                </Typography>
-            </Paper>
+          <Fragment>
+            <Grid container direction="column" justify="center" alignItems="center" style={{minHeight:'80vh'}}>
+                <Grid item xs={12}>
+                <Paper style={boxStyle} elevation={5}>
+                    <Typography variant="h5" component="h3" align="center">
+                    “We travel not to escape life, but for life not to escape us.”
+                    </Typography>
+                    <Typography variant="caption" align="right">
+                    Anonymous
+                    </Typography>
+                </Paper>
+                </Grid>
             </Grid>
-            <Grid item xs={12}>
+            <Grid container direction="column" justify="center" alignItems="center" item xs={12}>
             <IconButton href="#">
                 <FontAwesomeIcon icon={faArrowCircleDown} size="2x" style={{color: '#B31212'}}></FontAwesomeIcon>
             </IconButton>
             </Grid>
-        </Grid>
+        </Fragment>
       );
     }
 }
