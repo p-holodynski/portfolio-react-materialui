1066306fe2196ef841c48b9a6c11a54e51ad5fbb
Patryk Holodynski <p.holodynski@gmail.com>
2019-04-16 15:43:40 +0100

    Checkedlist finished, section-two finished

--- src/components/section-two.js
+++ src/components/section-two.js
@@ -8,7 +8,7 @@ import Checklist from './Checklist';
 class SectionTwo extends Component {
     render(){
         return (
-            <div id='section-two' style={{height: '100vh', backgroundColor: '#d6dde3'}}>
+            <div id='section-two' style={{height: '105vh', backgroundColor: '#d6dde3'}}>
                 <Grid container item xs={12} justify="center" alignItems="center">
                     <Typography variant="h3" component="h3" align="center" style={{fontFamily: 'Roboto Slab', fontSize: "1.5em", padding: "20px 20px 0px 20px"}}>
                     <span>Hi, my name is Patryk.</span><br/>
@@ -23,6 +23,9 @@ class SectionTwo extends Component {
                             <Typography variant="h5" align="center" style={{fontFamily: 'Playfair Display', padding: '5px 0px 10px 0px', fontSize: '1.75em'}}>
                             Programming
                             </Typography>
+                            <Typography component="p" align="center" style={{fontFamily: 'Roboto Slab', padding: '5px 0px 10px 0px', fontSize: '1em'}}>
+                            With a user-centered mindset,<br/> I try to develop intuitive and<br/> responsive applications.
+                            </Typography>
                             <NestedList />
                         </Paper>
                     </Grid>
@@ -31,6 +34,9 @@ class SectionTwo extends Component {
                         <Paper style={{backgroundColor: '#ffffff', padding: '15px',}} elevation={5}>
                             <Typography variant="h5" align="center" style={{fontFamily: 'Playfair Display', padding: '5px 0px 10px 0px', fontSize: '1.75em'}}>
                             Travelling
+                            </Typography>
+                            <Typography component="p" align="center" style={{fontFamily: 'Roboto Slab', padding: '5px 0px 10px 0px', fontSize: '1em'}}>
+                            I recently came back from SE Asia <br/> where I travelled for 6 months
                             </Typography>
                             <Checklist />
                         </Paper>
